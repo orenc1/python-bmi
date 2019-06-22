dbfb2acf8b2f91afca141ec3ca1ec5267114cd8f
orenc1 <oren5611@gmail.com>
2019-06-22 15:29:59 +0300

    adding input checks & corresponding error messages.

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request
+import json
 
 
 app = Flask(__name__)
+
+
+def is_number(num):
+    try:
+        float(num)
+    except ValueError:
+        return False
+    else:
+        return True
 
 
 @app.route('/')
@@ -15,6 +25,14 @@ def result():
         result_dict = request.form
         height = result_dict['height']
         weight = result_dict['weight']
+
+        illegal_values_list = []
+        if not is_number(height) or float(height) > 220 or float(height) < 40:
+            illegal_values_list.append('height')
+        if not is_number(weight) or float(weight) > 200 or float(weight) < 3:
+            illegal_values_list.append('weight')
+        if len(illegal_values_list) > 0:
+            return render_template('error.html', errors=json.dumps(illegal_values_list))
         try:
             height = float(height)/100
             weight = float(weight)
@@ -25,4 +43,4 @@ def result():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=8088)
+    app.run(debug=True, host='0.0.0.0', port=8088)
